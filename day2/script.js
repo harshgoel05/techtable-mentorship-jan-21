c258dfc969a30b9e823d9264119ab09edb31874f
harshgoel05 <harshgoel05@gmail.com>
2021-01-10 21:54:24 +0530

    day 2 content

---
+++ day2/script.js
@@ -0,0 +1,35 @@
+/*
+
+1. Build a website (Portfolio) :  Minimum Components : 5 (2 days)
+    0. Navbar 
+    1. Homepage
+    2. About You
+    3. Skills
+    4. Projects 
+    5. Hobbies/Hall of Fame
+    3. Contact us - console log all form fields 
+    4. Footer
+
+2. Upload on Github using TERMINAL only. Regular Commits  - 5mins/commit 
+
+3. Host it using Github Pages / Netlify  (Netlify is optional but intresting) - 15mins
+
+4. Work on Jaavascript 
+    4.1 variables ( difference b/w let const and var)  - 10mins
+    4.2 DOM manipulation using Javascript - 2 hours
+    4.3 Functions in Javascript [with return and non returning functions] - 45mins
+
+5. Revise Github - 2hrs 
+
+*/
+
+/*
+
+10-01 : Javascript & Github
+11-01 : Github and start building website
+13-01 09:00PM : Upload and Host website
+
+
+Deadline : 13-01 09:00PM [96 HOURS]
+
+*/
